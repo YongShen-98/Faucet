5c92fdfac811295ff30d9c1d828153bc708915e0
YongShen <940236834@qq.com>
2023-04-04 17:10:48 +0800

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import { ethers } from "ethers";
+import faucetContract from "./ethereum/faucet"
+
 
 function App() {
   const [walletAddress, setWalletAddress] = useState("");
-
+  const [signer, setSigner] = useState();
+  const [fcContract, setFcContract] = useState();
+  const [transactionData, setTransactionData] = useState("");
+ 
   useEffect(() => {
     getCurrentWalletConnected();
     addWalletListener();
@@ -12,10 +18,15 @@ function App() {
   const connectWallet = async () => {
     if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
       try {
-        /* MetaMask is installed */
-        const accounts = await window.ethereum.request({
-          method: "eth_requestAccounts",
-        });
+        /*get provider */
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        /* get accounts */
+        const accounts = await provider.send("eth_requestAccounts", [])
+        /*get signer */
+        setSigner(provider.getSigner());
+        /* local contract instance */
+        setFcContract(faucetContract(provider));
+
         setWalletAddress(accounts[0]);
         console.log(accounts[0]);
       } catch (err) {
@@ -30,10 +41,15 @@ function App() {
   const getCurrentWalletConnected = async () => {
     if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
       try {
-        const accounts = await window.ethereum.request({
-          method: "eth_accounts",
-        });
+        /*get provider */
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        /* get accounts */
+        const accounts = await provider.send("eth_accounts", [])
         if (accounts.length > 0) {
+          /*get signer */
+        setSigner(provider.getSigner());
+        /* local contract instance */
+          setFcContract(faucetContract(provider))
           setWalletAddress(accounts[0]);
           console.log(accounts[0]);
         } else {
@@ -61,12 +77,25 @@ function App() {
     }
   };
 
+  const getRVTHandler = async()=>{
+    try {
+      const fcContractWithSigner = fcContract.connect(signer);
+      const resp = await fcContractWithSigner.requestTokens();
+      setTransactionData(resp.hash);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+
+
+
   return (
     <div>
       <nav className="navbar">
         <div className="container">
           <div className="navbar-brand">
-            <h1 className="navbar-item is-size-4">Ocean Token (OCT)</h1>
+            <h1 className="navbar-item is-size-4">RIVER Token (RVT)</h1>
           </div>
           <div id="navbarMenu" className="navbar-menu">
             <div className="navbar-end is-align-items-center">
@@ -90,8 +119,8 @@ function App() {
       <section className="hero is-fullheight">
         <div className="faucet-hero-body">
           <div className="container has-text-centered main-content">
-            <h1 className="title is-1">Faucet</h1>
-            <p>Fast and reliable. 50 OCT/day.</p>
+            <h1 className="title is-1">Faucet on Sepolia testnet</h1>
+            <p>Fast and reliable. 50 RVT/min.</p>
             <div className="box address-box">
               <div className="columns">
                 <div className="column is-four-fifths">
@@ -99,10 +128,11 @@ function App() {
                     className="input is-medium"
                     type="text"
                     placeholder="Enter your wallet address (0x...)"
+                    defaultValue={walletAddress}
                   />
                 </div>
                 <div className="column">
-                  <button className="button is-link is-medium">
+                  <button className="button is-link is-medium" onClick={getRVTHandler}>
                     GET TOKENS
                   </button>
                 </div>
@@ -110,7 +140,10 @@ function App() {
               <article className="panel is-grey-darker">
                 <p className="panel-heading">Transaction Data</p>
                 <div className="panel-block">
-                  <p>transaction data</p>
+                  <p> {transactionData
+                      ? `Transaction hash: ${transactionData}`
+                      : "--"}
+                  </p>
                 </div>
               </article>
             </div>
